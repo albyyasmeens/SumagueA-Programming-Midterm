46e8cfcddc6d23ee11efce36e0ecfa66b7ae051c
albyyasmeens <156489829+albyyasmeens@users.noreply.github.com>
2024-02-19 18:04:41 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Programming 120 Midterm");
+Console.WriteLine("Basic Calculator");
+int a = 7;
+int b = 4;
+int c = 3;
+
+int result1 =  a +  b +  c; Console.WriteLine("a + b + c =" + result1 + ",");
+int result2 =  a -  b -  c; Console.WriteLine("a - b - c =" + result2 + ",");
+int result3 = ( a *  b) * c; Console.WriteLine("(a * b) * c =" + result3 + ",");
+int result4 = ( a /  b) /  c; Console.WriteLine("(a / b) / c =" + result4 + ",");
+Console.WriteLine("Equation 1 Result:" + result1);
+Console.WriteLine("Equation 2 result:" + result2);
+Console.WriteLine("Equation 3 result:" + result3);
+Console.WriteLine("Equation 4 result:" + result4);
+
+Console.WriteLine("Finance Calculator");
+Console.WriteLine ("Geometry Calculator");
+int d = 5;
+int result6= d ^ 2; Console.WriteLine("d ^ 2 ="+ result6 + ",");
+
+
